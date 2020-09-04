db50dfb50b1e82437d0ac009d378ec88616e2193
vishalrindhe <rindhevishal32@gmail.com>
2020-09-05 01:07:18 +0530

    12.2 import HttpClient n inject .HttpClientModule

--- tdf/src/app/enrollment.service.ts
+++ tdf/src/app/enrollment.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
-
+/**import HttpClient and
+ * inject it in constructor as (" constructor(private _http: HttpClient) {}") */
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EnrollmentService {
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
 }
